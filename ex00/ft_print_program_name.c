5582354da0e7f6d87480486552542f5775dce984
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-12-06 19:53:12 +0100

    added ex00

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void	ft_print_program_name(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	ft_print_program_name(argv[0]);
+}
